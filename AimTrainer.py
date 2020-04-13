c51cfa8cac4eec2ed514774ab52e7bc373a50760
Jackson <jdsikes@suddenlink.net>
2020-04-13 14:42:33 -0500

    Descriptions of each class

--- AimTrainer.py
+++ AimTrainer.py
@@ -1,20 +1,25 @@
-import tkinter as tk
+import Tkinter as tk
 import RPi.GPIO as GPIO
 from pygame import *
 
 
 
 class setupInputs(object):
+    #set up the GPIO pins for each photoresistor, and the two buttons that control the lazer
+    #measure the average value of each photoresistor and set that as a set point
     print "Tyler's class"
     pass
 
 
 class Graphics(canvas):
+    #set up the GUI with pygame? or tkinter
+    #use inputs from calculations to move the red dot around the screen based on values
     print "Jackson's class"
     pass
 
 
 class Calculations(object):
+    #use values measured from the photoresistors and setpoint to calculate where the lazer is
     print "Keith's class"
     pass
 
