0e711954213a156a0605fe72f351f10bbaf8a00c
Jackson <jdsikes@suddenlink.net>
2020-04-13 14:18:06 -0500

    Added Calculations class

--- AimTrainer.py
+++ AimTrainer.py
@@ -1,15 +1,21 @@
 import tkinter as tk
+import RPi.GPIO as GPIO
+from pygame import *
 
 
 
-class inputs(object):
+class setupInputs(object):
+    print "Tyler's class"
     pass
 
 
 class Graphics(canvas):
+    print "Jackson's class"
     pass
 
 
-
+class Calculations(object):
+    print "Keith's class"
+    pass
 
 
