dbbd1e5e5dc75308aaa5d00bf69b59fb0b571127
Jackson <jdsikes@suddenlink.net>
2020-04-30 20:47:53 -0500

    Testing arrays

--- TraingingP2.py
+++ TraingingP2.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+import Tkinter as tk
 import RPi.GPIO as GPIO
 from pygame import *
 import time
@@ -9,14 +9,25 @@ import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 from time import sleep
 
-# Remember to use Python 3, specifically 3.5.3 but I"m sure any 3 will work.
+# Remember to use Python 3, specifically 3.5.5 but I"m sure any 3 will work.
 # MCP3008 Library https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+
+
 
 # This is a class that creates each Photoresistor as an object. Each photoresistor
 # has a placement and a value. The placement is "topright, topmid, etc..."
 class Photoresistors(object):
     def __init__(self, value = 0):
         self.value = value
+
+
+# Set up the GPIO pins for each photoresistor, and the two buttons that control the lazer
+# Measure the average value of each photoresistor and set that as a set point
+##print ("Tyler's class")
+##GPIO.setmode(GPIO.BOARD) 
+##GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+##GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 # These just setup inputs and outputs for the Pi to communicate with the MCP3008 chip.
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -35,9 +46,16 @@ pr6 = AnalogIn(mcp, MCP.P5)
 pr7 = AnalogIn(mcp, MCP.P6)
 pr8 = AnalogIn(mcp, MCP.P7)
 
+
+
 #create a function that reads the values at each photoresistor
 def Reading():
+
+    lists = []
     while True:
+
+        #empty list
+        
         # These continuously update each Photoresistor object their light value from the array
         # in an infinite loop.
         topleft = pr1.value
@@ -49,19 +67,48 @@ def Reading():
         btmleft = pr7.value
         btmmid = pr8.value
 
+        Array = [topleft, topmid, topright, midleft, bullseye, midright, btmleft, btmmid]
+
+
+        #waits for a value to go over a threshold and collects each photoresistor data
+        #and stores it into an array
+        for value in Array:
+            if value >= 5000:
+                i = 0
+                while i < 4:
+                    topleft = pr1.value
+                    topmid = pr2.value
+                    topright = pr3.value
+                    midleft = pr4.value
+                    bullseye = pr5.value
+                    midright = pr6.value
+                    btmleft = pr7.value
+                    btmmid = pr8.value
+
+                    Array = [topleft, topmid, topright, midleft, bullseye, midright, btmleft, btmmid]
+                    lists.append(Array)
+                    
+                    sleep (.5)
+                    
+                    i += 1
+
+                Array = lists
+                break
+                    
+            
         # Just print statements for each for Tyler's testing purposes. 
         print('Pin 1: ', topleft)
         print('Pin 2: ', topmid)
         print('Pin 3: ', topright)
         print('Pin 4: ', midleft)
         print('Pin 5: ', bullseye)
-        print('Pin 6: ', midright)
+        print('Pin 6: ', midrighte)
         print('Pin 7: ', btmleft)
         print('Pin 8: ', btmmid)
         sleep(1)
 
         # Creates the array with all Photoresistor objects. 
-        Array = [topleft, topmid, topright, midleft, bullseye, midright, btmleft, btmmid]
+        
 
     return Array
 
@@ -86,8 +133,10 @@ try:
     
     array = Reading()
 
+    print (array)
+
 except KeyboardInterrupt:
-    print(array)
+   
     GPIO.cleanup()
 
 
